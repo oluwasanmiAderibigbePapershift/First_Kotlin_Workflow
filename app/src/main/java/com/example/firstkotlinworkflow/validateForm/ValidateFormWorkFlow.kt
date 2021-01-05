1e531fd135aeb8395f7a1df069ea2fd84837ad13
Oluwasanmi Aderibigbe <73818629+oluwasanmiAderibigbePapershift@users.noreply.github.com>
2021-01-05 05:27:13 +0100

    Update ValidateFormWorkFlow.kt

--- app/src/main/java/com/example/firstkotlinworkflow/validateForm/ValidateFormWorkFlow.kt
+++ app/src/main/java/com/example/firstkotlinworkflow/validateForm/ValidateFormWorkFlow.kt
@@ -62,6 +62,7 @@ object ValidateFormWorkFlow : StatefulWorkflow<Unit, ValidateFormWorkFlow.FormSt
     }
 
     //TODO [oa_5-01-2020] This code is wrong. If only the last field is valida, then the entire form would be seen as valid. find a way to fix this.
+    //Add a valid state for every field?
     private fun onSubmitTapped() = action {
 
         state = if(state.firstName.isNotBlank()){
